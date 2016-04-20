c8cb4e52980b4db73a3cf32c4ca70771dffb506b
ray <ray@dac.tw>
2016-04-20 12:52:17 +0800

    要試試看 ZbarCam 可否

---
+++ QR.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+def start_cam():
+    while True:
+        #Initializes an instance of Zbar to the commandline to detect barcode data-strings.
+        p=os.popen('/usr/bin/zbarcam --prescale=300x200','r')
+        #Barcode variable read by Python from the commandline.
+        print("Please Scan a QRcode to begin...")
+        barcode = p.readline()
+        barcodedata = str(barcode)[8:]
+
+        if barcodedata:
+            print("{0}".format(barcodedata))
+            #Kills the webcam window by executing the bash file 
+            os.system("/home/pi/Desktop/kill.sh")
+
+start_cam()
