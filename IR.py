17358270805ec8752a5bd8d6d20f3853aebbff9e
ray <ray@dac.tw>
2016-04-20 12:15:43 +0800

    第一次加

---
+++ IR.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#!-*- coding: utf-8 -*-
+
+import time
+import sys
+import RPi.GPIO as GPIO
+
+# 使用 BCM GPIO 取代實際接腳號碼
+GPIO.setmode(GPIO.BCM)
+# 定義樹莓派連接到 PIR 的接腳
+PIN_PIR = 18
+STB_TIME = 3
+
+try:
+    
+    GPIO.setup(PIN_PIR, GPIO.IN)
+    print "\n",
+    for i in range(0, STB_TIME):
+        print "      Waitting for PIR to stable, %d sec\r" %(STB_TIME - i),
+        sys.stdout.flush()
+        time.sleep(1)
+    
+    print "\n"
+    print "  READY ...",
+    print "\n"
+    
+    # 按下 Ctrl + C 離開
+    while True:
+        if(GPIO.input(PIN_PIR)):
+            print "THE PIR SENSOR DETECTED MOVEMENT           \r",
+            sys.stdout.flush()
+            time.sleep(3)   # 此時間的設定必須要大於 PIR 的延遲時間
+        else:
+            print "PIR sensor get ready for movement detection\r",            
+    
+except KeyboardInterrupt:    
+    print "\n\n  Quit!  \n"
---
+++ relay.py
@@ -0,0 +1,58 @@
+import time
+import sys
+import RPi.GPIO as GPIO
+
+# 使用 BCM GPIO 取代實際接腳號碼
+GPIO.setmode(GPIO.BCM)
+# 定義樹莓派連接到 PIR 的接腳
+RELAY_INP1 = 24 # GPIO #24 
+RELAY_INP2 = 25 # GPIO #25
+
+GPIO.setup(RELAY_INP1, GPIO.OUT)
+GPIO.setup(RELAY_INP2, GPIO.OUT)
+
+GPIO.output(RELAY_INP1, True)
+GPIO.output(RELAY_INP2, True)
+
+while True:
+    print("\nUsage: relay(1 or 2) on/off(1 or 0), ex. 1 0 (relay 1 off) \n")
+    print("3 0 all off, 3 1 all on, 0 0 to exit\n")
+    print("relay turn on/off: ({0} {1}): ".format(relay, onoff))
+    relay = input("")
+    onoff = input("")
+    print("relay turn on/off: ({0} {1}): ".format(relay, onoff))
+    sys.stdin.flush()
+    
+    if onoff == 0:
+        onoff = True
+    elif onoff == 1:
+        onoff = False
+    else:
+        printf("keyin the worng relay status!\n")
+        prog_exit()
+        
+    if relay == 0:
+        prog_exit()
+    elif relay == 1:
+        GPIO.output(RELAY_INP1, onoff)
+    elif relay == 2:
+        GPIO.output(RELAY_INP2, onoff)
+    elif relay == 3:
+        GPIO.output(RELAY_INP1, onoff)
+        GPIO.output(RELAY_INP2, onoff)
+    else:
+        print("Keyin the wrong relay number!\n")
+        prog_exit()        
+    
+def prog_exit():
+    GPIO.output(RELAY_INP1, True)
+    GPIO.output(RELAY_INP2, True)
+
+    what = GPIO.cleanup()
+    print what
+    // 關閉 bcm2835 函式庫
+    if what:
+        printf("Failed to close the library, deallocating any allocated memory and closing /dev/mem\n")
+        printf("\nPress any key to exit...")
+        return 2
+    return 0
