8bbba26f8b0b4e5cfba0036980163442469aa5bc
cheetahray <ray608@gmail.com>
2019-02-14 20:20:45 +0800

    Sort 修正

--- CardEquity004.groovy
+++ CardEquity004.groovy
@@ -3,9 +3,6 @@ import java.util.HashSet;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.util.TreeMap;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import java.text.SimpleDateFormat;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.intumit.citi.backend.CardInfo;
@@ -23,7 +20,8 @@ import com.intumit.citi.Result;
 import com.intumit.citi.CitiDeep
 import org.apache.wink.json4j.JSONObject;
 import org.apache.wink.json4j.JSONArray;
-
+import org.apache.commons.lang.StringUtils;
+import com.intumit.solr.robot.RobotFormalAnswers;
 private Content newContent(Content.Type type, String text) {
     Content content = new Content();
     content.setType(type);
@@ -39,7 +37,18 @@ private Action newAction(Action.Type type, String text, String url) {
     return action;
 }
 
-private Column setColumn(Column column, CitiDeep detail) {
+private boolean checkBlkcd(String s, HashSet set)
+{
+    String[] arr = s.split("");
+    for(String ch: arr)
+    {
+        if(set.contains(ch))
+            return true;
+    }
+    return false;
+}
+
+private Column setColumn(Column column, CitiDeep detail, boolean isGray) {
     try {
         column.setImageUrl(detail.getImageUrl());
         Content content = new Content();
@@ -59,7 +68,20 @@ private Column setColumn(Column column, CitiDeep detail) {
         action.setText("了解更多");
         action.setUrl(detail.getKnowMore());
         column.addInternalActions(action);
-        column.addInternalActions(new Action(Action.Type.URL,"馬上申請",detail.getApplyNow(),null));
+        /*
+        System.out.print(detail.getApplyNow());
+        if(StringUtils.isBlank(detail.getApplyNow()) || detail.getApplyNow().startsWith("不提"))
+        {
+            column.addInternalActions(new Action(Action.Type.URL,detail.getApplyNow(),"","disabled"));
+        }
+        else if(isGray)
+        {
+            column.addInternalActions(new Action(Action.Type.URL,"馬上申請",detail.getApplyNow(),"disabled"));
+        }
+        else
+        {
+            column.addInternalActions(new Action(Action.Type.URL,"馬上申請",detail.getApplyNow(),null));
+        }*/
         return column;
     } catch (URISyntaxException e) {
         // TODO Auto-generated catch block
@@ -68,14 +90,21 @@ private Column setColumn(Column column, CitiDeep detail) {
     return null;
 }
 
+private String formalAns(String key)
+{
+    return RobotFormalAnswers.getAnswers(ctx.getTenant().getId(),key).get(0).toString();  
+}
+
 try {
     if (ctx.currentQuestion != null && ctx.currentQuestion.length() > 0) {
-        if(ctx.getRequestAttribute(CitiUtil.userid) != null) {
-                String UserID = ctx.getRequestAttribute(CitiUtil.userid);
+        JSONObject jsonobj = (JSONObject)ctx.getCtxAttr("_bundle");
+        System.out.print("http://twwsb-chatbot1u.apac.nsroot.net:9080/wise/qa-ajax.jsp?apikey=" + jsonobj.get("apikey")
+                  + "&id=" + URLEncoder.encode(jsonobj.get("id"), "UTF-8") + "&q=" + jsonobj.get("q") );
+        if(jsonobj.has("id")) {
                 CardInfo cardinfo = ctx.getCtxAttr(Result.Postfix.CARDINFO.toString());
                 if (cardinfo == null || cardinfo.getResult().getCode() != 0) 
                 {
-                    cardinfo = CitiUtil.getSmartMenu(UserID, Result.Postfix.CARDINFO.toString());
+                    cardinfo = CitiUtil.getSmartMenu(jsonobj.get("id"), Result.Postfix.CARDINFO.toString());
                     //ctx.setCtxAttr(Result.Postfix.CARDINFO.toString(),cardinfo);
                 }
                 else
@@ -86,7 +115,7 @@ try {
                     long diff = TimeUnit.SECONDS.convert(diffInMillies, TimeUnit.MILLISECONDS);  
                     if (diff > Integer.parseInt(CitiUtil.getProperties("cacheSecs","200")))
                     {
-                       cardinfo = CitiUtil.getSmartMenu(UserID, Result.Postfix.CARDINFO.toString());
+                       cardinfo = CitiUtil.getSmartMenu(jsonobj.get("id"), Result.Postfix.CARDINFO.toString());
                        ctx.setCtxAttr(Result.Postfix.CARDINFO.toString(),cardinfo);
                     }
                 }
@@ -96,17 +125,22 @@ try {
                 result.setMessage(cardinfo.getResult().getMessage());
                 String jsonInString = mapper.writeValueAsString(result);
                 ctx.response.put("Result", new JSONObject(jsonInString));
-                List places = Arrays.asList(Arrays.asList(false,new HashSet<>(CitiDeep.logos(1,1)),"807"), 
-                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos(2,3)),"558"), 
-                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos(4,5)),"554"),
+                List bigcome = CitiDeep.logos("大來");
+                HashSet set1 = new HashSet<>(Arrays.asList(CitiUtil.s1));
+                set1.addAll(bigcome);
+                HashSet set2 = new HashSet<>(Arrays.asList(CitiUtil.s2));
+                set2.addAll(bigcome);
+                List places = Arrays.asList(Arrays.asList(false,new HashSet<>(CitiDeep.logos("Prestige")),"807"), 
+                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos("寰旅尊尚")),"558"), 
+                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos("寰旅世界")),"554"),
                                             Arrays.asList(false,new HashSet<>(CitiDeep.logos(6,9)),"596"),
-                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos(10,11)),"421"),
-                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos(12,15)),"263"),
-                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos(16,19)),"492"),
-                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos(20,20)),"241")
+                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos("PChome")),"421"),
+                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos("饗樂")),"263"),
+                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos("超級")),"492"),
+                                            Arrays.asList(false,new HashSet<>(CitiDeep.logos("HappyGo")),"241")
                                            );
                 MessageCarousel msgcrl = new MessageCarousel();
-                msgcrl.setId(ctx.getCtxAttr("_bundle").get("id"));
+                msgcrl.setId("");
                 msgcrl.setType(Message.Type.CAROUSEL);
                 int tmInc = 0;
                 List<Info> infos = cardinfo.getInfos();
@@ -114,7 +148,7 @@ try {
                 for (Info info:infos) {
                     Column column = new Column();
                     CitiDeep detail = CitiDeep.alist(info.getLogo());
-                    if(detail != null)
+                    if(detail != null  && !set2.contains(info.getLogo()) && !checkBlkcd(info.getBlkcd(),set2))
                     {
                       for (List place: places)
                       {
@@ -124,19 +158,27 @@ try {
                               place.set(0,true);
                           }
                       }
-                      column.setImageText(info.getCardno().replaceFirst(".*(\\d{4})", "xxx\$1"));
-                      column.setTitle(detail.getTitle() + (info.getCcl().equals("N") ? CitiUtil.sharingQuota:CitiUtil.singleQuota ));
-                      String tmId = String.valueOf(detail.getId());
-                      if(tm.containsKey(tmId))
+                      if(set1.contains(info.getLogo()) || checkBlkcd(info.getBlkcd(),set1))
                       {
-                        tm.put(tmId + "!" + (tmId++), setColumn(column, detail));
+                          column.setTitle(detail.getTitle() + formalAns("alreadyCancel") );
                       }
                       else
                       {
-                        tm.put(tmId, setColumn(column, detail));
+                          column.setImageText(info.getCardno().replaceFirst(".*(\\d{4})", "···· \$1"));
+                          column.setTitle(detail.getTitle());
+                      }
+                      String tmId = String.valueOf(detail.getId());
+                      if(tm.containsKey(tmId))
+                      {
+                        tm.put(String.format("%3s", tmId) + String.valueOf(++tmInc), setColumn(column, detail, true));
+                      }
+                      else
+                      {
+                        tm.put(String.format("%3s", tmId) + String.valueOf(tmInc), setColumn(column, detail, true));
                       }
                     }
                 }
+                System.out.println(tm);
                 Iterator i = tm.entrySet().iterator();
                 while(i.hasNext()) {
                     Map.Entry me = (Map.Entry)i.next();
@@ -148,9 +190,9 @@ try {
                     {
                         Column column = new Column();
                         CitiDeep detail = CitiDeep.alist(place.get(2));
-                        column.setImageText("xxx0000");
+                        column.setImageText("");
                         column.setTitle(detail.getTitle().replace("白金", "").replace("悠遊", ""));
-                        msgcrl.addColumn(setColumn(column, detail));   
+                        msgcrl.addColumn(setColumn(column, detail, false));   
                     }
                 }
                 jsonInString = mapper.writeValueAsString(msgcrl);
