d289bd5969a555c1ae88bc57e8cf1337f359a8c5
cheetahray <ray608@gmail.com>
2019-02-15 19:48:25 +0800

    Finally

--- BillRecord006.groovy
+++ BillRecord006.groovy
@@ -1,9 +1,5 @@
 import java.util.List;
 import java.util.TreeMap;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.intumit.citi.backend.CardInfo;
 import com.intumit.citi.backend.Info;
@@ -28,7 +24,8 @@ import com.intumit.citi.Result;
 import com.intumit.citi.CitiDeep;
 import org.apache.wink.json4j.JSONObject;
 import org.apache.wink.json4j.JSONArray;
-
+import org.apache.commons.lang.StringUtils;
+import com.intumit.solr.robot.RobotFormalAnswers;
 private Content newContent(Content.Type type, String text) {
     Content content = new Content();
     content.setType(type);
@@ -36,11 +33,29 @@ private Content newContent(Content.Type type, String text) {
     return content;
 }
 
+private String formalAns(String key)
+{
+    return RobotFormalAnswers.getAnswers(ctx.getTenant().getId(),key).get(0).toString();  
+}
+
+private boolean checkBlkcd(String s, HashSet set)
+{
+    String[] arr = s.split("");
+    for(String ch: arr)
+    {
+        if(set.contains(ch))
+            return true;
+    }
+    return false;
+}
+
 try {
     if (ctx.currentQuestion != null && ctx.currentQuestion.length() > 0) {
-        if(ctx.getRequestAttribute(CitiUtil.userid) != null) {
-                String UserID = ctx.getRequestAttribute(CitiUtil.userid);
-                CardInfo cardinfo = CitiUtil.getSmartMenu(UserID, Result.Postfix.PYMRECORD.toString());
+        JSONObject jsonobj = (JSONObject)ctx.getCtxAttr("_bundle");
+        System.out.print("http://twwsb-chatbot1u.apac.nsroot.net:9080/wise/qa-ajax.jsp?apikey=" + jsonobj.get("apikey")
+                  + "&id=" + URLEncoder.encode(jsonobj.get("id"), "UTF-8") + "&q=" + jsonobj.get("q") );
+        if(jsonobj.has("id")) {
+                CardInfo cardinfo = CitiUtil.getSmartMenu(jsonobj.get("id"), Result.Postfix.PYMRECORD.toString());
                 ObjectMapper mapper = new ObjectMapper();
                 Result result = new Result();
                 result.setCode(cardinfo.getResult().getCode());
@@ -48,10 +63,8 @@ try {
                 String jsonInString = mapper.writeValueAsString(result);
                 ctx.response.put("Result", new JSONObject(jsonInString));
                 HashSet set1 = new HashSet<>(Arrays.asList(CitiUtil.s1));
-                set1.addAll(CitiDeep.logos(29,29));
-                MessageCarousel msgcrl = new MessageCarousel();
-                msgcrl.setId(ctx.getCtxAttr("_bundle").get("id"));
-                msgcrl.setType(Message.Type.CAROUSEL);
+                set1.addAll(CitiDeep.logos(15));
+                HashSet set2 = new HashSet<>(Arrays.asList(CitiUtil.s1));
                 int tmInc = 0;
                 List<Info> infos = cardinfo.getInfos();
                 TreeMap tm = new TreeMap();
@@ -60,69 +73,109 @@ try {
                     CitiDeep detail = CitiDeep.alist(info.getLogo());
                     if(detail != null)
                     {
+                      boolean blkcdLess0 = false;
                       try {
                           column.setImageUrl(detail.getImageUrl());
-                          column.setImageText(info.getCardno().replaceFirst(".*(\\d{4})", "xxx\$1"));
-                          column.setTitle(detail.getTitle() + (set1.contains(info.getLogo())?CitiUtil.alreadyCancel:""));
+                          column.setImageText(info.getCardno().replaceFirst(".*(\\d{4})", "···· \$1"));
+                          boolean alreadyCancel = ( set1.contains(info.getLogo()) || checkBlkcd(info.getBlkcd(), set1) );
+                          column.setTitle(detail.getTitle() + (alreadyCancel?formalAns("alreadyCancel"):""));
                           Content content = new Content();
-                          content.setText(detail.getTitle() + (set1.contains(info.getLogo())?CitiUtil.alreadyCancel:""));
+                          content.setText(detail.getTitle() + (alreadyCancel?formalAns("alreadyCancel"):""));
                           content.setType(Content.Type.GRID);
                           content.addHeader(new Header("繳款日",null));
-                          content.addHeader(new Header("完成日",null));
+                          content.addHeader(new Header("入帳日",null));
                           content.addHeader(new Header("金額","right"));
                           int newLineCnt = 0;
                           for (Pym pym:info.getPyms()) {
-                              Row row = new Row();
-                              Field field = new Field();
-                              field.setText(pym.getTxndate());
-                              row.addField(field);
-                              field = new Field();
-                              field.setText(pym.getDuedate());
-                              row.addField(field);
-                              field = new Field();
-                              field.setText("");
-                              row.addField(field);
-                              if (newLineCnt > 0)
-                                  row.setIsAlternatingRow(true);
-                              newLineCnt++;
-                              content.addRow(row);
-                              row = new Row();
-                              field = new Field();
-                              field.setText(pym.getTxndesc());
-                              row.addField(field);
-                              field = new Field();
-                              field.setText("");
-                              row.addField(field);
-                              field = new Field();
-                              field.setText(pym.getTxnamount());
-                              field.setIsBold(true);
-                              field.setAlign("right");
-                              row.addField(field);
-                              content.addRow(row);
+                              int txncode = ( StringUtils.isNumeric(pym.getTxncode())?Integer.parseInt(pym.getTxncode()):0 );
+                              if(txncode > 700 && txncode < 800)
+                              {
+                                  Row row = new Row();
+                                  Field field = new Field();
+                                  field.setText(pym.getTxndate());
+                                  row.addField(field);
+                                  field = new Field();
+                                  field.setText(pym.getDuedate());
+                                  row.addField(field);
+                                  field = new Field();
+                                  field.setText("");
+                                  row.addField(field);
+                                  if (newLineCnt > 0)
+                                      row.setIsAlternatingRow(true);
+                                  newLineCnt++;
+                                  content.addRow(row);
+                                  row = new Row();
+                                  field = new Field();
+                                  field.setText(pym.getTxndesc());
+                                  row.addField(field);
+                                  field = new Field();
+                                  field.setText("");
+                                  row.addField(field);
+                                  field = new Field();
+                                  field.setText(pym.getTxnamount());
+                                  field.setIsBold(true);
+                                  field.setAlign("right");
+                                  row.addField(field);
+                                  content.addRow(row);
+                              }
                           }
-                          column.addContent(content);
-
+                          if(newLineCnt != 0)
+                          {
+                              column.addContent(content);
+                          }
+                          else
+                          {   
+                              boolean isAB = checkBlkcd(info.getBlkcd(),set2);
+                              if(!isAB || ( isAB && StringUtils.isNotEmpty(info.getCurrBal()) &&
+                                 info.getCurrBal().matches(CitiUtil.isNumeric))                   
+                                )
+                              {
+                                  if(Integer.parseInt(info.getCurrBal()) > 0)
+                                      column.addContent( newContent(Content.Type.TEXT, formalAns("noNewPymRecord")) );
+                                  else
+                                      blkcdLess0 = true;
+                              }
+                          }
                       } catch (URISyntaxException e) {
                           // TODO Auto-generated catch block
                           e.printStackTrace();
                       }
-                      String tmId = String.valueOf(detail.getId());
-                      if(tm.containsKey(tmId))
+                      if(false == blkcdLess0)
                       {
-                        tm.put(tmId + "!" + (tmId++), column);
+                          String tmId = String.valueOf(detail.getId());
+                          if(tm.containsKey(tmId))
+                          {
+                              tm.put(String.format("%3s", tmId) + String.valueOf(++tmInc), column);
+                          }
+                          else
+                          {
+                              tm.put(String.format("%3s", tmId) + String.valueOf(tmInc), column);
+                          }
                       }
-                      else
-                      {
-                        tm.put(tmId, column);
-                      }
-                    }
+                   }
                 }
                 Iterator i = tm.entrySet().iterator();
-                while(i.hasNext()) {
-                    Map.Entry me = (Map.Entry)i.next();
-                    msgcrl.addColumn(me.getValue());
+                MessageCarousel msgcrl = null;
+                MessageText msgtxt = null;
+                if(i.hasNext())
+                {
+                    msgcrl = new MessageCarousel();
+                    msgcrl.setId("");
+                    msgcrl.setType(Message.Type.CAROUSEL);
+                    while(i.hasNext()) {
+                        Map.Entry me = (Map.Entry)i.next();
+                        msgcrl.addColumn(me.getValue());
+                    }
+                    jsonInString = mapper.writeValueAsString(msgcrl);
                 }
-                jsonInString = mapper.writeValueAsString(msgcrl);
+                else
+                {
+                    msgtxt = new MessageText();
+                    msgtxt.setId("");
+                    msgtxt.setType(Message.Type.TEXT);
+                    msgtxt.setText(formalAns("noNewPymRecord"));
+                    jsonInString = mapper.writeValueAsString(msgtxt);
+                }
                 ctx.response.put("Messages", new JSONArray("[" + jsonInString + "]"));
         }
     }
